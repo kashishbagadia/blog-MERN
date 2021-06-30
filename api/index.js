4bd90b9bf3fd81211c2f1865979728c89bad9d64
kashishbagadia <kashishbagadiamd06@gmail.com>
2021-06-30 16:42:21 +0530

    mern

--- api/index.js
+++ api/index.js
@@ -1,15 +1,34 @@
 const connectDB = require("./config/db");
-
+const authRoute = require("./routes/auth");
 const userRoute = require("./routes/users");
+const postRoute = require("./routes/posts");
+const categoryRoute = require("./routes/categories");
+const multer = require("multer")
 
 const express = require("express");
 const app = express();
-const authRoute = require("./routes/auth");
+
 app.use(express.json());
 
 connectDB();
+
+const storage = multer.diskStorage({
+    destination:(req,file,cb)=>{
+        cb(null,"images")
+    },filename:(req,file,cb)=>{
+        cb(null,req.body.name);
+    }
+}) 
+
+const upload = multer({ storage:storage});
+app.post("/api/upload", upload.single("file"),(req,res)=>{
+    res.status(200).json("File has been uploaded!");
+});
+
 app.use("/api/auth", authRoute)
 app.use("/api/users", userRoute)
+app.use("/api/posts", postRoute)
+app.use("/api/categories", categoryRoute)
 
 app.listen("5000", () => {
     console.log("Backend is running!!")
