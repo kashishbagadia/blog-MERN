f0b2dfd09cc649827b63cd6d4c461764ab99ad13
Kashish Bagadia <kashishbagadiamd06@gmail.com>
2021-07-03 23:32:21 +0530

    Update SinglePost.jsx

--- client/src/components/singlePost/SinglePost.jsx
+++ client/src/components/singlePost/SinglePost.jsx
@@ -1,41 +1,63 @@
-import { useEffect, useState } from "react";
-import { useLocation } from "react-router-dom"
-import "./singlePost.css"
-import axios from "axios"
+import { useContext, useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+import "./singlePost.css";
+import axios from "axios";
 import { Link } from "react-router-dom";
+import { Context } from "../../context/Context";
 
 export default function SinglePost() {
-    const location = useLocation()
-    const path = location.pathname.split("/")[2];
-    const[post,setPost] = useState({})
+  const location = useLocation();
+  const path = location.pathname.split("/")[2];
+  const [post, setPost] = useState({});
+  const PF = "http://localhost:5000/images/";
+  const { user } = useContext(Context);
 
-    useEffect(()=>{
-        const getPost = async () => {
-            const res = await axios.get("/posts/"+path);
-            setPost(res.data)
-        };
-        getPost();
-    },[path])
-    return (
-        <div className="singlePost">
-            <div className="singlePostWrapper">
-                {post.photo && (
-                    <img src={post.photo} alt="" className="singlePostImg" />
-                )}
-                
-                <h1 className="singlePostTitle">{post.title}
-                    <div className="singlePostEdit">
-                        <i className="singlePostIcon far fa-edit"></i>
-                        <i className="singlePostIcon far fa-trash-alt"></i>
-                    </div>
-                </h1>
-                <div className="singlePostInfo">
-                    <span className="singlePostAuthor">Author: <Link to={`/?user=${post.username}`} className="link"><b>{post.username}</b></Link></span>
-                    <span className="singlePostAuthor">{new Date(post.createdAt).toDateString()}</span>
-                </div>
-                <p className="singlePostDesc">{post.desc}
-                </p>
+  useEffect(() => {
+    const getPost = async () => {
+      const res = await axios.get("/posts/" + path);
+      setPost(res.data);
+    };
+    getPost();
+  }, [path]);
+
+  const handleDelete = async () => {
+      try{
+          await axios.delete("posts/" + path)
+
+      }catch(err){
+          
+      }
+  }
+
+  return (
+    <div className="singlePost">
+      <div className="singlePostWrapper">
+        {post.photo && (
+          <img src={PF + post.photo} alt="" className="singlePostImg" />
+        )}
+
+        <h1 className="singlePostTitle">
+          {post.title}
+          {post.username === user?.username && (
+            <div className="singlePostEdit">
+              <i className="singlePostIcon far fa-edit"></i>
+              <i className="singlePostIcon far fa-trash-alt" onClick={handleDelete}></i>
             </div>
+          )}
+        </h1>
+        <div className="singlePostInfo">
+          <span className="singlePostAuthor">
+            Author:{" "}
+            <Link to={`/?user=${post.username}`} className="link">
+              <b>{post.username}</b>
+            </Link>
+          </span>
+          <span className="singlePostAuthor">
+            {new Date(post.createdAt).toDateString()}
+          </span>
         </div>
-    )
+        <p className="singlePostDesc">{post.desc}</p>
+      </div>
+    </div>
+  );
 }
